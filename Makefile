50b2b1d1531ced15021f2f661c60c87d57590837
elzekaj <elzekaj@student.42heilbronn.de>
2025-07-04 16:55:29 +0200

    Add: Implementation of ft_isprint.c

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC = gcc
 
 CFLAGS = -Wall -Wextra -Werror
 
-SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c
+SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c
 
 OBJS = ${SRC:.c=.o}
 
--- ft_isprint.c
+++ ft_isprint.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_libft.h                                         :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elzekaj <elzekaj@student.42.fr>            #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-07-04 14:09:35 by elzekaj           #+#    #+#             */
-/*   Updated: 2025-07-04 14:09:35 by elzekaj          ###   ########.fr       */
+/*   Created: 2025-07-04 14:54:38 by elzekaj           #+#    #+#             */
+/*   Updated: 2025-07-04 14:54:38 by elzekaj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
