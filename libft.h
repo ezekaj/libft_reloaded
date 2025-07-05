6c2cfd9e83bd56454ddc0b72db032cccf28bbe8b
elzekaj <elzekaj@student.42heilbronn.de>
2025-07-05 18:31:25 +0200

    Add: Implementation of ft_strchr.c

--- libft.h
+++ libft.h
@@ -26,6 +26,8 @@ int		ft_atoi(char *str);
 
 size_t	ft_strlen(const char *s);
 
+char	*ft_strchr(char *str, int c);
+
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
